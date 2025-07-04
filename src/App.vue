872691480d799679ad91c8ffd815dcfc4c8a6c7e
KarlaBedregal <kbedregalc@unsa.edu.pe>
2025-07-04 00:15:10 -0500

    uuuuuuu ssddwofwer

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <NavbarComponent />
+    <router-view />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NavbarComponent from '@/components/common/NavbarComponent.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    NavbarComponent
   }
 }
 </script>
@@ -19,8 +21,8 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 0;
+  padding: 0;
 }
 </style>
